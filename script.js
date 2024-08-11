2f8c566b1cba673493b239ee183aa9e43dd6c026
Brandon Arias Cjuno <brandon12arias@gmail.com>
2024-08-11 03:03:24 -0500

    mejoras

--- script.js
+++ script.js
@@ -15,3 +15,63 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+
+var btnLn = document.getElementById("btnLn");
+var btnInsta = document.getElementById("btnInsta");
+var btnContact = document.getElementById("btnContact");
+btnLn.onclick = function() {
+  window.location.assign("https://www.linkedin.com/company/fabula-kreatif/");
+}
+btnInsta.onclick = function() {
+  window.location.assign("https://www.instagram.com/ajansfabula?igsh=MWwyZ3E2YmowcnQyaQ%3D%3D&utm_source=qr");
+}
+btnContact.onclick = function() {
+  alert("Contact form building");
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const modelViewer = document.querySelector('model-viewer');
+  
+  let lastZoom = modelViewer.getCameraOrbit().radius;
+  
+
+  const adjustHotspots = () => {
+    const hotspots = document.querySelectorAll('button.annotation');
+    const currentZoom = modelViewer.getCameraOrbit().radius;
+
+    if (lastZoom !== currentZoom) {
+      const scaleFactor = 26 - currentZoom;
+      hotspots.forEach(hotspot => {
+        hotspot.style.width = `${1.3 * scaleFactor}em`; // Tamaño ajustado
+        hotspot.style.height = `${1.3 * scaleFactor}em`; // Tamaño ajustado
+      });
+      lastZoom = currentZoom;
+    }
+  };
+
+  // Usar un bucle de actualización para ajustar los hotspots
+  const updateLoop = () => {
+    adjustHotspots();
+    requestAnimationFrame(updateLoop);
+  };
+
+  updateLoop();
+});
+
+
+function esDispositivoMovil() {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  const esMovilPorUserAgent = /Mobi|Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(userAgent);
+  const esMovilPorTamaño = window.matchMedia('(max-width: 767px)').matches;
+  
+  return esMovilPorUserAgent || esMovilPorTamaño;
+}
+
+if (esDispositivoMovil()) {
+  alert("Estas en movil");
+} else {
+  alert("Estas en una computadora");
+}
